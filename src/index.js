9dbb09bc26a306ab3a4b067dbd9e46dfcd288a42
Vaneluuq <76055965+Vaneluuq@users.noreply.github.com>
2021-02-25 21:16:39 +0100

    se añadio funcion mul

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import validator from './validator.js';
+function primerPaso (numeroDeTarjeta) {
+    return numeroDeTarjeta.Value;
+}
+function luhnCheck() {
+    const multiplicacionP = primerPaso.toString().split("").map(Number);
 
-console.log(validator);
+    const sum = multiplicacionP
+    .map((digit,idx) => idx % 2 === multiplicacionP.length % 2 ? fixDouble(digit*2) : digit).reduce((acc,digit) => acc +=digit,0);
+
+    return sum % 10 === 0;
+}
+function fixDouble (number) {
+    return number > 9 ? number - 9: number;
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,19 @@
-const validator = {
-  // ...
-};
+document.getElementById("pagar").addEventListener("click", comenzar);
+function comenzar() {
+ocultarMostrar("form","contenedor");
+}
+function ocultarMostrar (pantallaEncendida , pantallaApagada){
+    document.getElementById(pantallaApagada).style.display = "none";
+    document.getElementById(pantallaEncendida).style.display = "inline";
+}
 
-export default validator;
+
+const cantidadP= document.getElementById("cantidadProductos");
+const precioP = 15000;
+const btnCalcular = document.getElementById("pagar");
+
+btnCalcular.addEventListener("click", mul);
+function mul () {
+const multi = cantidadP.value*precioP;
+document.getElementById("valor").innerHTML= "El valor total a pagar es de $ "+ multi + " por favor ingrese la siguiente información para finalizar la compra ";
+}
