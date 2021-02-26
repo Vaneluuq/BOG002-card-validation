c2d0433364ec0fb8904bfc2bf0197bf1694978ae
Natnarben <Natnarben@users.noreply.github.com>
2021-02-25 21:50:15 -0500

    realice algunas modificaciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-import validator from './validator.js';
+/*Función para llamar el numero de la tarjeta ingresado*/
+numeroDeTarjetaInput =  document.getElementById("numeroDeTarjeta");
 
-console.log(validator);
+/*Función para guardar el numero de la tarjeta de crédito*/
+function getNumeroDeTarjetaInput(){
+    return numeroDeTarjetaInput.value;
+}   
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function validar(n){
+    let digitos = n.toString().split("").map(Number);
+    
+    
+    if(digitos.length % 2 === 0) {
+       digitos = digitos.map((digitos, idx) => idx % 2 === 0 ? digitos * 2 : digitos);  
+       digitos = digitos.map((digitos, idx) => idx % 2 === 1 ? digitos * 2 : digitos); 
+    }
+
+    digitos = digitos.map(digitos => digitos > 9 ? digitos - 9 : digitos);
+
+    const sum = digitos.reduce((acc, digitos) => acc += digitos, 0);
+} */
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,8 @@
-const validator = {
-  // ...
-};
-
-export default validator;
+document.getElementById("pagar").addEventListener("click", comenzar);
+function comenzar() {
+ocultarMostrar("form","contenedor");
+}
+function ocultarMostrar (pantallaEncendida , pantallaApagada){
+    document.getElementById(pantallaApagada).style.display = "none";
+    document.getElementById(pantallaEncendida).style.display = "inline";
+}
